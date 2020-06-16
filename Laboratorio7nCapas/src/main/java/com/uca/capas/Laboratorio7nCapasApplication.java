6b8d8df9b0dc21a160f713f3b23efa6a62fdfa80
AlexisMejia <00090718@uca.edu.sv>
2020-06-16 15:05:24 -0600

    firts commit

---
+++ Laboratorio7nCapas/src/main/java/com/uca/capas/Laboratorio7nCapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio7nCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio7nCapasApplication.class, args);
+	}
+
+}
---
+++ Laboratorio7nCapas/src/main/java/com/uca/capas/domain/Estudiante.java
@@ -0,0 +1,97 @@
+package com.uca.capas.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(schema = "public", name = "estudiante")
+public class Estudiante {
+
+	@Id
+	@Column(name = "id_estudiante")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigoEstudiante;
+
+	@Size(message = "El campo no debe contener más de 30 caracteres", max = 30)
+	@NotEmpty(message = "*Campo obligatorio")
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Size(message = "El campo no debe contener más de 30 caracteres", max = 30)
+	@NotEmpty(message = "*Campo obligatorio")
+	@Column(name = "apellido")
+	private String apellido;
+
+	@NotNull(message = "*Campo obligatorio")
+	@Min(value = 18, message = "Mayor a 18 años")
+	@Column(name = "edad")
+	private Integer edad;
+
+	@Column(name = "estado")
+	private Boolean estado;
+
+	public Integer getCodigoEstudiante() {
+		return codigoEstudiante;
+	}
+
+	public void setCodigoEstudiante(Integer codigoEstudiante) {
+		this.codigoEstudiante = codigoEstudiante;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		apellido = apellido;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		edad = edad;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		estado = estado;
+	}
+
+	public Estudiante() {
+
+	}
+
+	public String getEstadoDelegate() {
+		if (this.estado == null)
+			return "";
+		else {
+			return estado == true ? "Activo" : "Inactivo";
+		}
+	}
+}
